47161939b6a740d9ffd9ffababe77795837925e2
Davit Barbakadze <jayarjo@gmail.com>
2013-07-23 16:12:11 +0400

    Auto-parse .details files to Demo Reference in README.md.

---
+++ Jakefile.js
@@ -0,0 +1,27 @@
+/* global jake:true, desc:true, task:true, complete:true, require:true, console:true, process:true */
+/* jshint unused:false, laxcomma:true */
+var fs = require('fs')
+, path = require('path')
+, util = require('util')
+, glob = require('glob')
+, yaml = require('yamljs')
+;
+
+desc("Add demos reference to README.md.");
+task("default", [], function () {
+
+	glob("**/demo.details", function (err, files) {
+		var ref = ''
+		, readme = fs.readFileSync('README.md').toString();
+
+		files.forEach(function(file) {
+			var contents = fs.readFileSync(file).toString().replace(/(^[\s\S]*\-\-\-|\.\.\.[\s\S]*$)/g, '')
+			, demo = yaml.parse(contents);
+			ref += util.format('* [%s](%s)\n', demo.name, demo.url);
+		});
+
+		ref = '`<!--ref-->`\n' + ref + '`<!--/ref-->`';
+		fs.writeFileSync('README.md', readme.replace(/`<!\-\-ref\-\->`[\s\S]*`<!\-\-\/ref\-\->`/, ref));
+	});
+});
+
